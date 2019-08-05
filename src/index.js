245db519caaf889afc53400a937d745b23e2beec
Jan-Henrik Hempel <jhempel1.ext@it4ipm.de>
2019-08-05 10:39:22 +0200

    Add locale support.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const moment = require('moment');
 // Capture the a or the p in the meridiem
 const meridiemRegEx = new RegExp('(a{1,2}|p).?m{1}?.?', 'i');
 let defaultFormat = 'YYYY';
+let defaultLocale = 'fr';
 
 const getMeridiemFormat = (dateString, format = defaultFormat) =>
   moment(dateString).format(format).replace(meridiemRegEx, '$1.m.');
 
 const getFormat = (dateString, format = defaultFormat) =>
   moment(dateString).format(format);
+
+const getLocale = (locale = defaultLocale) =>
+  moment.locale(locale); 
 
 function getFilter (dateString, ...args) {
   const isMeridiemOnly = typeof args[0] === 'boolean';
@@ -33,6 +37,9 @@ module.exports = getFilter;
 
 // Set user-specified default format for date
 module.exports.setDefaultFormat = format => (defaultFormat = format);
+
+// Set user-specified default format for date
+module.exports.setLocale = locale => (defaultLocale = locale);
 
 // Add filter to nunjucks environment
 module.exports.install = (env, customName) => {
